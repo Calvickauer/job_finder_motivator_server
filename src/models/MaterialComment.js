297fb000f5c6ca0b2692a29c042128dff39c2afe
dkobrin <shdwkat999@yahoo.com>
2023-02-27 15:27:38 -0500

    fixed comment creation bug, and added owner_name to the comment

--- src/models/MaterialComment.js
+++ src/models/MaterialComment.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const matCommentSchema = new mongoose.Schema({
     schema_v: {type: Number, default: 1},
     owner: {type: Schema.Types.ObjectId, ref: "User"},
+    owner_name: {type: String},
     title: {type: String},
     content: {type: String},
     // comments: [{type: Schema.Types.ObjectId, ref: "MaterialComment"}],
